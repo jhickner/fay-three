5237d74d657d80feefff2799215618b2958105ab
Jason Hickner <jhickner@gmail.com>
2014-06-19 13:36:10 -0700

    tweaked test

--- tests.hs
+++ tests.hs
@@ -9,29 +9,36 @@ import Language.Fay.JQuery
 
 main :: Fay ()
 main = ready $ do
-  cam <- mkPerspectiveCamera 75 1.333 1 10000
+  w <- getWindowWidth
+  h <- getWindowHeight
+  cam <- mkPerspectiveCamera 75 (w/h) 1 10000
   scene <- mkScene
   r <- mkCanvasRenderer
-  geo <- mkCubeGeometry 200 200 200
+  geo <- mkCubeGeometry 500 500 500
   mat <- mkMeshBasicMaterial 0xff0000 False
   mesh <- mkMesh geo mat
   setZ cam 1000
   addObject3D scene mesh
-  setRendererSize r 640 480
+  setRendererSize r w h
   addRenderer r
   animate r scene cam mesh
 
 animate :: Renderer -> Object3D -> Object3D -> Object3D -> Fay ()
 animate r scene cam mesh = f
-  where 
-    f = do
-      requestAnimationFrame f
-      incRotationX mesh 0.01
-      incRotationY mesh 0.02
-      render r scene cam
+  where f = do
+          requestAnimationFrame f
+          incRotationX mesh 0.01
+          incRotationY mesh 0.02
+          render r scene cam
 
 addRenderer :: Renderer -> Fay ()
 addRenderer = ffi "document.body.appendChild(%1.domElement)"
+
+getWindowWidth :: Fay Double
+getWindowWidth = ffi "window.innerWidth"
+
+getWindowHeight :: Fay Double
+getWindowHeight = ffi "window.innerHeight"
 
 log :: Foreign a => a -> Fay ()
 log = ffi "console.log(%1)"
